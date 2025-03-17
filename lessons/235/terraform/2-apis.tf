e9563bcf176453de3e9fd67c6736f7e27dfbe2f4
BryanYZ <50778021+BryanYZ@users.noreply.github.com>
2025-03-16 20:01:07 -0400

    updated

--- lessons/235/terraform/2-apis.tf
+++ lessons/235/terraform/2-apis.tf
@@ -21,4 +21,10 @@
 
 #   disable_on_destroy = false
 # }
-#
+
+# resource "google_project_service" "api" {
+#   for_each = toset(local.apis)
+#   service  = each.key
+
+#   disable_on_destroy = false
+# }
